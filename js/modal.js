dd43bb6889758733f856d5dd79753be5704444be
Wilson Júnior <willnapolitano21@gmail.com>
2022-01-12 16:08:04 -0300

    Create a 'toggle()' function to open and close the modal

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,10 @@
-// close modal
 
+const modal_overlay = document.querySelector('.modal-overlay')
 let modal = document.querySelector("#more-calculators")
 let open_modal = document.querySelector("#img-1")
 let close_modal = document.querySelector("#close")
 
-open_modal.addEventListener("click", function(){
-    modal.style.display = "initial"
-})
+const toggle = () => {
+    modal_overlay.classList.toggle('active')
+}
 
-close_modal.addEventListener("click", function(){
-    modal.style.display = "none"
-})
