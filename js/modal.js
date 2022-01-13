ba6b96984433f066f7c1d9d1aa890ce774d99a96
Wilson Júnior <willnapolitano21@gmail.com>
2022-01-13 15:04:50 -0300

    Remove unnecessary lines of code

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,5 @@
 
 const modal_overlay = document.querySelector('.modal-overlay')
-let modal = document.querySelector("#more-calculators")
-let open_modal = document.querySelector("#img-1")
-let close_modal = document.querySelector("#close")
 
 const toggle = () => {
     modal_overlay.classList.toggle('active')
